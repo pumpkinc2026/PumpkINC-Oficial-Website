3ffee1dcc8489934f0b3fb6ac6e003a13f1ce934
pumpkinc2026 <pumpkinc2026@gmail.com>
2026-01-27 22:23:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable:true,   
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    autoplay: {
+      delay: 3000,            
+      disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:4,
+    spaceBetween: 30,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    breakpoints : {
+        0:{
+            slidesPerView:1
+        },
+        520: {
+            slidesPerView:2
+        },
+        950: {
+            slidesPerView:3
+        }
+    }
+});
